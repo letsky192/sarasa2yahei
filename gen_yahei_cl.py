e18dbaef3dbd5bf2b70ebc1836bec81c2c0f4466
RichardLuo0 <22096448+RichardLuo0@users.noreply.github.com>
2024-02-03 20:35:55 +0800

    change to SarasaMonoSC

--- gen_yahei_cl.py
+++ gen_yahei_cl.py
@@ -3,14 +3,14 @@ import shutil as fs
 
 
 def gen_yahei_regular_cl():
-    fs.copy(utils.DOWNLOAD_DIR + '/SarasaUiCL-Regular.ttf', utils.DOWNLOAD_DIR + '/SarasaUiCL-Regular-ui.ttf')
+    fs.copy(utils.DOWNLOAD_DIR + '/SarasaMonoSC-Regular.ttf', utils.DOWNLOAD_DIR + '/SarasaMonoSC-Regular-ui.ttf')
 
-    font = utils.open_font(utils.DOWNLOAD_DIR + '/SarasaUiCL-Regular.ttf')
+    font = utils.open_font(utils.DOWNLOAD_DIR + '/SarasaMonoSC-Regular.ttf')
     utils.remove_gasp(font)
     utils.set_cleartype(font)
     utils.set_yahei_regular_names(font)
 
-    font_ui = utils.open_font(utils.DOWNLOAD_DIR + '/SarasaUiCL-Regular-ui.ttf')
+    font_ui = utils.open_font(utils.DOWNLOAD_DIR + '/SarasaMonoSC-Regular-ui.ttf')
     utils.remove_gasp(font_ui)
     utils.set_cleartype(font_ui)
     utils.set_yahei_regular_ui_names(font_ui)
@@ -18,14 +18,14 @@ def gen_yahei_regular_cl():
     font.generateTtc(utils.OUTPUT_DIR_CL + '/msyh.ttc', font_ui, ttcflags = ('merge'), layer = 1)
 
 def gen_yahei_light_cl():
-    fs.copy(utils.DOWNLOAD_DIR + '/SarasaUiCL-Light.ttf', utils.DOWNLOAD_DIR + '/SarasaUiCL-Light-ui.ttf')
+    fs.copy(utils.DOWNLOAD_DIR + '/SarasaMonoSC-Light.ttf', utils.DOWNLOAD_DIR + '/SarasaMonoSC-Light-ui.ttf')
 
-    font = utils.open_font(utils.DOWNLOAD_DIR + '/SarasaUiCL-Light.ttf')
+    font = utils.open_font(utils.DOWNLOAD_DIR + '/SarasaMonoSC-Light.ttf')
     utils.remove_gasp(font)
     utils.set_cleartype(font)
     utils.set_yahei_light_names(font)
 
-    font_ui = utils.open_font(utils.DOWNLOAD_DIR + '/SarasaUiCL-Light-ui.ttf')
+    font_ui = utils.open_font(utils.DOWNLOAD_DIR + '/SarasaMonoSC-Light-ui.ttf')
     utils.remove_gasp(font_ui)
     utils.set_cleartype(font_ui)
     utils.set_yahei_light_ui_names(font_ui)
@@ -33,14 +33,14 @@ def gen_yahei_light_cl():
     font.generateTtc(utils.OUTPUT_DIR_CL + '/msyhl.ttc', font_ui, ttcflags = ('merge'), layer = 1)
 
 def gen_yahei_bold_cl():
-    fs.copy(utils.DOWNLOAD_DIR + '/SarasaUiCL-Bold.ttf', utils.DOWNLOAD_DIR + '/SarasaUiCL-Bold-ui.ttf')
+    fs.copy(utils.DOWNLOAD_DIR + '/SarasaMonoSC-Bold.ttf', utils.DOWNLOAD_DIR + '/SarasaMonoSC-Bold-ui.ttf')
 
-    font = utils.open_font(utils.DOWNLOAD_DIR + '/SarasaUiCL-Bold.ttf')
+    font = utils.open_font(utils.DOWNLOAD_DIR + '/SarasaMonoSC-Bold.ttf')
     utils.remove_gasp(font)
     utils.set_cleartype(font)
     utils.set_yahei_bold_names(font)
 
-    font_ui = utils.open_font(utils.DOWNLOAD_DIR + '/SarasaUiCL-Bold-ui.ttf')
+    font_ui = utils.open_font(utils.DOWNLOAD_DIR + '/SarasaMonoSC-Bold-ui.ttf')
     utils.remove_gasp(font_ui)
     utils.set_cleartype(font_ui)
     utils.set_yahei_bold_ui_names(font_ui)
