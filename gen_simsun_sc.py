388b6934c74365847a83844ff84f17915796b7a4
RichardLuo0 <22096448+RichardLuo0@users.noreply.github.com>
2024-02-03 21:12:16 +0800

    change to SarasaUISC

--- gen_simsun_sc.py
+++ gen_simsun_sc.py
@@ -3,14 +3,14 @@ import shutil as fs
 
 
 def gen_simsun_sc():
-    fs.copy(utils.DOWNLOAD_DIR + '/SarasaMonoSC-Regular.ttf', utils.DOWNLOAD_DIR + '/SarasaMonoSC-Regular-new.ttf')
+    fs.copy(utils.DOWNLOAD_DIR + '/SarasaUISC-Regular.ttf', utils.DOWNLOAD_DIR + '/SarasaUISC-Regular-new.ttf')
 
-    font = utils.open_font(utils.DOWNLOAD_DIR + '/SarasaMonoSC-Regular.ttf')
+    font = utils.open_font(utils.DOWNLOAD_DIR + '/SarasaUISC-Regular.ttf')
     utils.remove_gasp(font)
     utils.set_cleartype(font)
     utils.set_simsun_names(font)
 
-    font_ui = utils.open_font(utils.DOWNLOAD_DIR + '/SarasaMonoSC-Regular-new.ttf')
+    font_ui = utils.open_font(utils.DOWNLOAD_DIR + '/SarasaUISC-Regular-new.ttf')
     utils.remove_gasp(font_ui)
     utils.set_cleartype(font_ui)
     utils.set_new_simsun_names(font_ui)
@@ -18,7 +18,7 @@ def gen_simsun_sc():
     font.generateTtc(utils.OUTPUT_DIR_SC + '/simsun.ttc', font_ui, ttcflags = ('merge'), layer = 1)
 
 def gen_simsun_ext_sc():
-    font = utils.open_font(utils.DOWNLOAD_DIR + '/SarasaMonoSC-Regular.ttf')
+    font = utils.open_font(utils.DOWNLOAD_DIR + '/SarasaUISC-Regular.ttf')
     utils.remove_gasp(font)
     utils.set_cleartype(font)
     utils.set_simsun_ext_names(font)
