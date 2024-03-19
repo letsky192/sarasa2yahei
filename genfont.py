57d6f64371e50b1b102d366a034737eb04747276
letsky192 <73059745+letsky192@users.noreply.github.com>
2024-03-19 23:27:41 +0800

    Update genfont.py
    复原 xAvgCharWidth

--- genfont.py
+++ genfont.py
@@ -7,19 +7,26 @@ def gen_sc(sarasa, sarasa_ui, prefmy_zh, prefmy_en,
             weight, weighthuman,
             out):
 
-    font = utils.open_font(utils.DOWNLOAD_DIR + '/' + sarasa)
+    file1 = utils.DOWNLOAD_DIR + '/' + sarasa
+    file2 = utils.DOWNLOAD_DIR + '/' + sarasa_ui
+    outfile = utils.OUTPUT_DIR_SC + '/' + out
+
+    font = utils.open_font(file1)
     utils.set_font_info_sc(font, prefmy_zh, prefmy_en,
                         cprt_zh, cprt_en, subfmy,
                         weight, weighthuman)
 
-    font_ui = utils.open_font(utils.DOWNLOAD_DIR + '/' + sarasa_ui)
+    font_ui = utils.open_font(file2)
     utils.set_font_info_sc(font_ui, prefmy_zh_ui, prefmy_en_ui,
                         cprt_zh, cprt_en, subfmy,
                         weight, weighthuman)
 
-    font.generateTtc(utils.OUTPUT_DIR_SC+'/'+out, font_ui, ttcflags = ('merge'), layer = 1)
+    font.generateTtc(outfile, font_ui, ttcflags = ('merge'), layer = font.activeLayer)
     utils.close_font(font)
     utils.close_font(font_ui)
+    XACW_Bytes1 = utils.readXACW(file1)
+    XACW_Bytes2 = utils.readXACW(file2)
+    utils.patchXACW(outfile, XACW_Bytes1, XACW_Bytes2)
 
 # set_font_info_sc(font, prefmy_zh, prefmy_en,
 #                 cprt_zh, cprt_en, subfmy,
@@ -27,23 +34,29 @@ def gen_sc(sarasa, sarasa_ui, prefmy_zh, prefmy_en,
 
 # Style = ('Regular', 'Italic', 'Bold', 'Bold Italic')
 
-
 def gen_tc(sarasa, sarasa_ui, prefmy_zh, prefmy_en,
             prefmy_zh_ui, prefmy_en_ui,
             cprt_zh, cprt_en, subfmy,
             weight, weighthuman,
             out):
 
-    font = utils.open_font(utils.DOWNLOAD_DIR + '/' + sarasa)
+    file1 = utils.DOWNLOAD_DIR + '/' + sarasa
+    file2 = utils.DOWNLOAD_DIR + '/' + sarasa_ui
+    outfile = utils.OUTPUT_DIR_TC + '/' + out
+
+    font = utils.open_font(file1)
     utils.set_font_info_tc(font, prefmy_zh, prefmy_en,
                         cprt_zh, cprt_en, subfmy,
                         weight, weighthuman)
 
-    font_ui = utils.open_font(utils.DOWNLOAD_DIR + '/' + sarasa_ui)
+    font_ui = utils.open_font(file2)
     utils.set_font_info_tc(font_ui, prefmy_zh_ui, prefmy_en_ui,
                         cprt_zh, cprt_en, subfmy,
                         weight, weighthuman)
 
-    font.generateTtc(utils.OUTPUT_DIR_TC+'/'+out, font_ui, ttcflags = ('merge'), layer = 1)
+    font.generateTtc(outfile, font_ui, ttcflags = ('merge'), layer = font.activeLayer)
     utils.close_font(font)
     utils.close_font(font_ui)
+    XACW_Bytes1 = utils.readXACW(file1)
+    XACW_Bytes2 = utils.readXACW(file2)
+    utils.patchXACW(outfile, XACW_Bytes1, XACW_Bytes2)
